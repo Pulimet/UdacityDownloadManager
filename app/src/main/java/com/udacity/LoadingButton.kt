00c7eded98d8e62135a4e03b2a3b3d31a0063e7e
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-07-28 06:37:32 +0300

    Set LoadingButton.kt initial animation

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,49 +2,89 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.DecelerateInterpolator
+import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val RECT_PADDING = 30f
+    }
+
     private var bgColor = Color.YELLOW
     private var textColor = Color.BLACK
     private var textSizeFloat = 70f
-    private var text = "Nothing"
+    private var btnCurrentText = "Nothing"
+    private var btnLoadingText = "We are loading"
+    private var btnRegularText = ""
     private var widthSize = 0
     private var heightSize = 0
+    private var hCenter = 0
     private var textX = 0f
     private var textY = 0f
+    private var animInitialValue = 0
 
+    // Rect
     private var bgRect = Rect(0, 0, 100, 100)
+    private var bgRectAnim = Rect(0, 0, 100, 100)
+    private val arcRect: RectF = RectF(0f, 0f, 0f, 0f)
+    private val textBound = Rect()
 
+    // Paints
     private val bgPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val bgAnimPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = ContextCompat.getColor(context, R.color.bg_anim)
+    }
+    private val arcPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = ContextCompat.getColor(context, R.color.arc)
+    }
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         textAlign = Paint.Align.CENTER
     }
-
-    private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { p, old, new ->
 
     }
 
     init {
+        getDataFromAttributes(context, attrs)
+        setPaintsValues()
+        startLoadingAnimationNow()
+    }
+
+    private fun startLoadingAnimationNow() {
+        ValueAnimator.ofInt(0, 101).apply {
+            startDelay = 500
+            duration = 4000
+            interpolator = DecelerateInterpolator()
+            addUpdateListener { valueAnimator ->
+                animInitialValue = valueAnimator.animatedValue as Int
+                invalidate()
+            }
+        }.start()
+    }
+
+
+    private fun getDataFromAttributes(context: Context, attrs: AttributeSet?) {
         context.theme.obtainStyledAttributes(attrs, R.styleable.LoadingButton, 0, 0).apply {
             try {
                 bgColor = getColor(R.styleable.LoadingButton_bgColor, Color.YELLOW)
                 textColor = getColor(R.styleable.LoadingButton_textColor, Color.BLACK)
-                text = getString(R.styleable.LoadingButton_text) ?: "Nothing"
+                btnCurrentText = getString(R.styleable.LoadingButton_text) ?: "Nothing"
+                btnRegularText = btnCurrentText
             } finally {
                 recycle()
             }
         }
+    }
+
+    private fun setPaintsValues() {
         bgPaint.color = bgColor
         textPaint.color = textColor
         textPaint.textSize = textSizeFloat
@@ -54,19 +94,58 @@ class LoadingButton @JvmOverloads constructor(
         val minWidth: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         widthSize = resolveSizeAndState(minWidth, widthMeasureSpec, 1)
         heightSize = resolveSizeAndState(MeasureSpec.getSize(widthSize), heightMeasureSpec, 0)
+        hCenter = widthSize / 2
+
+        // Background
         bgRect.right = widthSize
         bgRect.bottom = heightSize
+        bgRectAnim.bottom = heightSize
+        // Text
         textX = bgRect.centerX().toFloat()
         textY = (bgRect.centerY().toFloat() - ((textPaint.descent() + textPaint.ascent()) / 2))
+        // Arc
+        arcRect.set(
+            RECT_PADDING,
+            RECT_PADDING,
+            heightSize.toFloat(),
+            heightSize.toFloat() - RECT_PADDING
+        )
+
         setMeasuredDimension(widthSize, heightSize)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.apply {
-            drawRect(bgRect, bgPaint)
-            drawText(text, textX, textY, textPaint)
+            drawInitialBg()
+            drawInitialBgAnimation()
+            drawInitialText()
         }
+    }
+
+    private fun Canvas.drawInitialBg() {
+        drawRect(bgRect, bgPaint)
+    }
+
+    private fun Canvas.drawInitialBgAnimation() {
+        when (animInitialValue) {
+            1 -> btnCurrentText = btnLoadingText
+            101 -> btnCurrentText = btnRegularText
+        }
+        if (animInitialValue in 1..100) {
+            bgRectAnim.right = widthSize * animInitialValue / 100
+            drawRect(bgRectAnim, bgAnimPaint)
+            val sweepAngle = 360f * animInitialValue / 100
+
+            arcRect.left = hCenter + textBound.width() / 2 + RECT_PADDING * 2
+            arcRect.right = arcRect.left +  heightSize.toFloat() - RECT_PADDING * 2
+            drawArc(arcRect, 0f, sweepAngle, true, arcPaint)
+        }
+    }
+
+    private fun Canvas.drawInitialText() {
+        drawText(btnCurrentText, textX, textY, textPaint)
+        textPaint.getTextBounds(btnCurrentText, 0, btnCurrentText.length - 1, textBound)
     }
 
 }
